8561af2703f562063af98f368f2ea11f38e8a40f
Neal Sales-Griffin <nealsalesgriffin@gmail.com>
2012-02-09 11:21:21 -0600

    created a new boat party app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CaBoatParty::Application.config.secret_token = 'f8601e5b60b6af5a186a15add0a90573b3da33c6a0c0b9a70217057a4e17395d797acf1592ac7d97b061ab47b159a708cac4d42926dd2f2f0c3b1b0d84b583d9'
---
+++ test/functional/boat_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class BoatControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
